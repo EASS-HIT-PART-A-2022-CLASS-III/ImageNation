bbe07632d351937b9d65fdc188f7d5ee11c99360
Royga2 <galiliroy@gmail.com>
2023-05-27 18:23:10 +0300

    speration started

--- app/backend/images/main.py
+++ app/backend/images/main.py
@@ -1,16 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import models
 from database import engine
 from routers import image, user, authentication
+from starlette.middleware.base import BaseHTTPMiddleware
+import time
 
-
-app = FastAPI()
+app = FastAPI(title="Image-Nation", version="0.1.0")
 
 models.Base.metadata.create_all(engine)
 
 app.include_router(authentication.router)
 app.include_router(user.router)
 app.include_router(image.router)
+
+
+class ProccessingTimeMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = time.time() - start_time
+        response.headers["X-Process-Time"] = str(process_time)
+        return response
+
+
+app.add_middleware(ProccessingTimeMiddleware)
 
 
 # @app.get(
